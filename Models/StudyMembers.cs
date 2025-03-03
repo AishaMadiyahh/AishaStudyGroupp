bfefa6a833a9625c78173f0c5c34068e21f28490
aisha madiyah <madiyahaisha@gmail.com>
2025-03-03 18:46:33 +0200

    small tweaks

---
+++ Models/StudyMembers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AishaStudyGroupp.Models
+{
+	public class StudyMembers
+	{
+        public string StudentNumber { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}
